0990fdfbeebc821f2bf85a3a98ace722d5109e9a
BiFoS <jon89071@gmail.com>
2024-01-04 15:21:39 +0900

    :tada: First commit

---
+++ src/test/kotlin/com/bifos/fospay/FospayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bifos.fospay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FospayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
