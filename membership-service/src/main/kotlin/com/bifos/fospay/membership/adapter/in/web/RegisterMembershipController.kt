c14a70ca50e42f6eaa2acbcf5c3a636a5527660e
BiFoS <jon89071@gmail.com>
2024-01-08 15:49:41 +0900

    :white_check_mark: 테스트에 응답 도 검증하도록 추가

--- membership-service/src/main/kotlin/com/bifos/fospay/membership/adapter/in/web/RegisterMembershipController.kt
+++ membership-service/src/main/kotlin/com/bifos/fospay/membership/adapter/in/web/RegisterMembershipController.kt
@@ -4,7 +4,7 @@ import com.bifos.fospay.membership.application.port.`in`.RegisterMembershipComma
 import com.bifos.fospay.membership.application.port.`in`.RegisterMembershipUseCase
 import common.WebAdapter
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -18,7 +18,7 @@ class RegisterMembershipController(
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     @PostMapping("/membership/register")
-    fun registerMembership(@RequestBody requestBody: RegisterMembershipRequest): String {
+    fun registerMembership(@RequestBody requestBody: RegisterMembershipRequest): ResponseEntity<*> {
         // request~~
         // request => Command
         // Usecase ~~ (request x, command)
@@ -30,8 +30,8 @@ class RegisterMembershipController(
             isCorp = requestBody.isCorp
         )
 
-        registerMembershipUseCase.registerMembership(command)
+        val membership = registerMembershipUseCase.registerMembership(command)
 
-        return "register"
+        return ResponseEntity.ok().body(membership)
     }
 }
--- membership-service/src/test/kotlin/com/bifos/fospay/membership/RegisterMembershipControllerTest.kt
+++ membership-service/src/test/kotlin/com/bifos/fospay/membership/RegisterMembershipControllerTest.kt
@@ -1,6 +1,7 @@
 package com.bifos.fospay.membership
 
 import com.bifos.fospay.membership.adapter.`in`.web.RegisterMembershipRequest
+import com.bifos.fospay.membership.domain.Membership
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -23,11 +24,22 @@ class RegisterMembershipControllerTest @Autowired constructor(
     @DisplayName("유저가 가입된다")
     fun testRegisterMembership() {
         val request = RegisterMembershipRequest("name", "address", "email", true)
+
+        val memberShip = Membership.generate(
+            Membership.MembershipId(1),
+            Membership.MembershipName(request.name),
+            Membership.MembershipEmail(request.email),
+            Membership.MembershipAddress(request.address),
+            Membership.MembershipIsValid(true),
+            Membership.MembershipIsCorp(request.isCorp),
+        )
+
         mockMvc.perform(
             MockMvcRequestBuilders.post("/membership/register")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
         )
             .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().string(objectMapper.writeValueAsString(memberShip)))
     }
 }
